e4d3e70fb2efdf63d366ce2542627e6471f8025e
Marcin <koral90@poczta.fm>
2012-03-28 12:13:47 +0200

    Male zmiany w pliku about.php . Ten commit jest glownie po to, zeby sprawdzic, czy git dziala.

--- kino/protected/views/site/pages/about.php
+++ kino/protected/views/site/pages/about.php
@@ -6,13 +6,17 @@ $this->breadcrumbs=array(
 ?>
 <h1>O nas</h1>
 
-<p>Cinema-City Sp. z o.o. to małe kino we Wrocławiu istniejące od 2010 roku. Kino cieszy się największym zaufaniem i zadowoleniem Klientów we Wrocławiu ze względu na niskie ceny i nietypowe filmy występujące w repertuarze. Obecnie Cinema-City posiada 8 ekranów, ponad 2 tys. foteli.
+<p>Cinema-town Sp. z o.o. to małe kino we Wrocławiu istniejące od 
+2010 roku. Kino cieszy się największym zaufaniem i zadowoleniem 
+Klientów we Wrocławiu ze względu na niskie ceny i nietypowe filmy 
+występujące w repertuarze. Obecnie Cinema-town posiada 8 ekranów, 
+ponad 2 tys. foteli.
 Spółka wyróżnia się na rynku własną autorską koncepcją biznesową. Koncentruje się na rozwoju przede wszystkim na rynku osiedlowym..</p>
 
 <h1>Struktura organizacyjna</h1>
 
 <p>Kierownictwo:</p>
-Dyrektor Generalny - mgr inż. Grzegorz Paplaka<br />
+Dyrektor Generalny - mgr inż. Grzegorz Papkala<br />
 Dyrektor Zarządzający - Maciej Wójcik<br />
 Dyrektor Finansowy - Marcin Koralewski<br />
 <br />
