eb7e2d17b91f677a64f22b1ae9d29fe971e2c589
Marcin <koral90@poczta.fm>
2012-04-11 12:58:52 +0200

    Podszlifowanie strony glownej

--- kino/protected/views/site/index.php
+++ kino/protected/views/site/index.php
@@ -2,18 +2,25 @@
 <br />
 <h1>Kino <i><?php echo CHtml::encode(Yii::app()->name); ?></i> serdecznie zaprasza!</h1>
 <br />
-<p>Premiery tygodnia:</p>
-<ul>
-	<li>Igrzyska śmierci</li>
-</ul>
-<br />
-<p>Polecamy:</p>
-<ul>
-	<li>Spadkobiercy</li>
-</ul>
-<br />
-<p>Zapowiedzi:</p>
-<ul>
-	<li>Gniew tytanów</li>
-</ul>
+<div style="width: 70%; float: left;">
+	<h2>Premiery tygodnia: </h2>
+		<h3>Igrzyska śmierci</h3>
+		<iframe width="560" height="315" src="http://www.youtube.com/embed/b31rlxpLrMc" frameborder="0" allowfullscreen></iframe>
+	<br />
+	<hr width="560" align="left" />
+	<h2>Polecamy:</h2>
+		<h3>Spadkobiercy</h3>
+		<iframe width="560" height="315" src="http://www.youtube.com/embed/yzv7TAUD3dU" frameborder="0" allowfullscreen></iframe>
+	<br />
+	<hr width="560" align="left"/>
+	<h2>Zapowiedzi:</h2>
+		<h3>Gniew tytanów</h3>
+		<iframe width="560" height="315" src="http://www.youtube.com/embed/UFeL6lf0aM0" frameborder="0" allowfullscreen></iframe>
+		<br /><br />
+</div>
+<div style="width: 30%; float: right;">
+	<h2>Promocja!</h2>
+	<br />
+	<img src="/kino/images/reklama.jpg" alt="Promocja!">
+</div>
 <br /><br />
